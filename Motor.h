fed07bf0d82a74f4ff53c67e153e4f59c43b102b
DonBoxGit <donbox@yandex.ru>
2022-06-07 16:46:14 +0300

    alignment text

--- Motor.h
+++ Motor.h
@@ -7,23 +7,23 @@ class Motor {
   public:
 	Motor  (const uint8_t step, const uint8_t dir, const uint8_t enb);
 	~Motor ();
-	void enableOn 	  (void);
+	void enableOn	  (void);
 	void enableOff	  (void);
 	void setForward   (void);
-    void setReverse   (void);
-    void stop         (void);  
+	void setReverse   (void);
+	void stop		  (void); 
 	bool getEnable	  (void);
 	bool getDirection (void);
-	void execute	  	(void);
+	void execute	  (void);
   
   private:
-    void run          (void);
+    void run		  (void);
 	
 	uint8_t step_pin;
 	uint8_t dir_pin;
 	uint8_t enable_pin;
-	   bool	enable;
-	   bool	dir;
+	   bool enable;
+	   bool dir;
 };
 
 #endif MOTOR_H

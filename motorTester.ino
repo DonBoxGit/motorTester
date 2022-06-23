4dfe473703fd583595860166d4b7c1ac6955be14
DonBoxGit <donbox@yandex.ru>
2022-06-23 14:19:52 +0300

    add Start menu

--- motorTester.ino
+++ motorTester.ino
@@ -1,13 +1,13 @@
-/* Motor Tester ver. 1.1
- *   Stepper Motor: MITSUMI M42SP-7
- *   Step Angle   : 7.5 degree
- *
- * Driver: A4988
- *    ENBL: LOW - On, HIGH - Off
- *    DIR : HIGH - ClockWise, LOW - CounterClockWise 
- */
+/* Motor Tester ver. 1.2
+     Stepper Motor: MITSUMI M42SP-7
+     Step Angle   : 7.5 degree
 
-#include "config.h"                                                                                                                     
+   Driver: A4988
+      ENBL: LOW - On, HIGH - Off
+      DIR : HIGH - ClockWise, LOW - CounterClockWise
+*/
+
+#include "config.h"
 #include <Adafruit_SSD1306.h>
 #include <EncButton.h>
 #include "Motor.h"
@@ -26,12 +26,12 @@ EncButton<EB_TICK, DT, SLK, SW> encoder;
 EncButton<EB_TICK, BUTTON_RIGHT_PIN> right_btn(INPUT_PULLUP);
 EncButton<EB_TICK, BUTTON_LEFT_PIN > left_btn (INPUT_PULLUP);
 EncButton<EB_TICK, BUTTON_RESET_PIN> reset_btn(INPUT_PULLUP);
- 
+
 void setup() {
-  #ifdef DEBUG
+#ifdef DEBUG
   Serial.begin(9600);
-  #endif
-  
+#endif
+
   initInterrupt();
   //pinMode(BUTTON_RIGHT_PIN, INPUT_PULLUP);
   //pinMode(BUTTON_LEFT_PIN,  INPUT_PULLUP);
@@ -39,22 +39,20 @@ void setup() {
   pinMode(TERM_SW_PIN_1, INPUT_PULLUP);
   pinMode(TERM_SW_PIN_2, INPUT_PULLUP);
   pinMode(SPEAKER_PIN,   OUTPUT);
-  
+
   encoder.setEncType(0);			      // Full Step Type Ecnoder
   attachInterrupt(0, isr, CHANGE);  // SLK
   attachInterrupt(1, isr, CHANGE);  // DT
-  
+
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_I2C_ADDR)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_I2C_ADDR)) {
     Serial.println(F("SSD1306 allocation failed."));
-    for(;;);
+    for (;;);
   }
-  display.clearDisplay();
+
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
+  startMenu();
 }
 
 void isr() {
@@ -62,61 +60,114 @@ void isr() {
 }
 
 uint8_t  subtrahend = 100;
-uint8_t  score      = 0;
+int8_t   score      = 0;
+bool blinkFlag = true;
 
 void loop() {
   encoder.tick();
-  right_btn.tick();
-  left_btn.tick();
-  reset_btn.tick();
-
-  if(encoder.right()) {
-    impulse += subtrahend;
-  }
-  else if(encoder.left()) {
-    if(impulse < MIN_) impulse = MIN_;
-    else impulse -= subtrahend;
-  }
-  if(encoder.click()) {
-    if(score == 2) {subtrahend = 100; score = 0;}
-    else           {subtrahend /= 10; score++;  }
-  }
-
-  if(right_btn.press()) {
-    motor.setForward();
-    motor.enableOn();
-  } else if (right_btn.release()) {
-    motor.enableOff();
+  
+  if (encoder.left()) {
+    score--;
+    if (score < 0) score = 1;
+    startMenu();
   }
   
-  if(left_btn.press()) {
-    motor.setReverse();
-    motor.enableOn();
-  } else if (left_btn.release()) {
-    motor.enableOff();
+  if (encoder.right()) {
+    score++;
+    if (score > 1) score = 0;
+    startMenu();
   }
-  
-  if(reset_btn.click()) steps = 0;
-  
-  if(!digitalRead(TERM_SW_PIN_1) || !digitalRead(TERM_SW_PIN_2)) motor.stop();
 
-  if(drawTimer.ready()) draw();
+  if (encoder.click()) {
+    while (true) {
+      encoder.tick();
+      right_btn.tick();
+      left_btn.tick();
+      reset_btn.tick();
 
-  motor.execute();
+      if (encoder.right()) {
+        impulse += subtrahend;
+      }
+      
+      else if (encoder.left()) {
+        if (impulse < MIN_) impulse = MIN_;
+        else impulse -= subtrahend;
+      }
+      
+      if (encoder.click()) {
+        if (score == 2) { subtrahend = 100; score = 0; }
+        else            { subtrahend /= 10; score++;   }
+      }
+
+      if (right_btn.press()) {
+        motor.setForward();
+        motor.enableOn();
+      } else if (right_btn.release()) {
+        motor.enableOff();
+      }
+
+      if (left_btn.press()) {
+        motor.setReverse();
+        motor.enableOn();
+      } else if (left_btn.release()) {
+        motor.enableOff();
+      }
+
+      if (reset_btn.click()) steps = 0;
+
+      if (!digitalRead(TERM_SW_PIN_1) || !digitalRead(TERM_SW_PIN_2)) motor.stop();
+
+      if (drawTimer.ready()) mainMenu();
+
+      motor.execute();
+    }
+  }
 }
 
-void draw(void) {
-  encoder.tick();
+void startMenu(void) {
   display.clearDisplay();
-  
+
+    //display.drawRoundRect(0, 0, 128, 32, 0, WHITE);
+  display.setCursor(26, 5);
+  display.print(F("SELECT DRIVER"));
+
+  switch (score) {
+    case 0:
+      display.fillRoundRect(25, 16, 35, 11, 3, WHITE);
+      display.setCursor(28, 18);
+      display.setTextColor(BLACK);
+      display.print(F("A4988"));
+      display.setTextColor(WHITE);
+      
+      display.setCursor(68, 18);
+      display.print(F("TB6600"));
+    break;
+
+    case 1:
+      display.setCursor(28, 18);
+      display.print(F("A4988"));
+      display.fillRoundRect(64, 16, 42, 11, 3, WHITE);
+      display.setTextColor(BLACK);
+      display.setCursor(68, 18);
+      display.print(F("TB6600"));
+      display.setTextColor(WHITE);
+    break;
+  }
+  display.display();
+}
+
+
+void mainMenu(void) {
+  display.clearDisplay();
+
   display.setCursor(3, 10);
   display.print(F("Speed: "));
   display.setCursor(40, 10);
   display.print(impulse);
   display.setCursor(68, 10);
   display.print(F("-"));
-  
-  switch(score){
+
+  switch (score) {
     case 0:
       display.fillRoundRect(77, 8, 20, 11, 3, WHITE);
       display.setTextColor(BLACK);
@@ -127,8 +178,8 @@ void draw(void) {
       display.print("10");
       display.setCursor(118, 10);
       display.print(F("1"));
-    break;
-    
+      break;
+
     case 1:
       display.setCursor(78, 10);
       display.print("100");
@@ -139,8 +190,8 @@ void draw(void) {
       display.setTextColor(WHITE);
       display.setCursor(118, 10);
       display.print(F("1"));
-    break;
-    
+      break;
+
     case 2:
       display.setCursor(78, 10);
       display.print("100");
@@ -151,16 +202,17 @@ void draw(void) {
       display.setCursor(118, 10);
       display.print(F("1"));
       display.setTextColor(WHITE);
-    break;
+      break;
   }
-    display.setCursor(55, 25);
-    display.print(F("Steps: "));
-    display.setCursor(92, 25);
-    display.print(steps);
-
-  if(!motor.getEnable()){
-    if(motor.getDirection()){
-      if(rStatusTimer.ready()){
+  display.setCursor(55, 25);
+  display.print(F("Steps: "));
+  display.setCursor(92, 25);
+  display.print(steps);
+  
+  if (!motor.getEnable()) {
+    if (rStatusTimer.ready()) blinkFlag = !blinkFlag;
+    if (motor.getDirection()) {
+      if (blinkFlag) {
         tone(SPEAKER_PIN, FREQUENCY_SP, DURATION_SP);
         display.fillRoundRect(1, 21, 46, 11, 3, WHITE);
         display.setTextColor(BLACK);
@@ -168,21 +220,20 @@ void draw(void) {
         display.print("FORWARD");
       }
     } else {
-      if(rStatusTimer.ready()){
+      if (blinkFlag) {
         tone(SPEAKER_PIN, FREQUENCY_SP, DURATION_SP);
         display.fillRoundRect(1, 21, 46, 11, 3, WHITE);
         display.setTextColor(BLACK);
         display.setCursor(4, 23);
         display.print("REVESRE");
-
       }
     }
   } else {
-    if(!digitalRead(TERM_SW_PIN_1)) {
+    if (!digitalRead(TERM_SW_PIN_1)) {
       display.fillRoundRect(1, 21, 41, 11, 3, WHITE);
       display.setTextColor(BLACK);
       display.setCursor(4, 23);
-      display.print("TERM-1");      
+      display.print("TERM-1");
     } else if (!digitalRead(TERM_SW_PIN_2)) {
       display.fillRoundRect(1, 21, 41, 11, 3, WHITE);
       display.setTextColor(BLACK);
@@ -194,7 +245,7 @@ void draw(void) {
       display.setCursor(9, 23);
       display.print("STOP");
     }
-  } 
+  }
   display.setTextColor(WHITE);
   display.display();
 }

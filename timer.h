00b7995cb7cda29693695cc978d14e0925e5360e
DonBoxGit <donbox@yandex.ru>
2022-07-03 15:02:00 +0300

    init _tmr = 0

--- timer.h
+++ timer.h
@@ -1,10 +1,10 @@
 #ifndef TIMER_H
 #define TIMER_H
-//#include <Arduino.h>
+#include <Arduino.h>
 
 class Timer {
   private:
-    uint32_t _tmr;
+    uint32_t _tmr = 0;
     uint16_t _period;
   public:
     Timer(int period = 1000) : _period(period) {}
